fdce93e0b60fd67275e29c72fff2ac22344b8660
Egor <egruzdev14gmail.com>
2024-10-28 13:16:41 +0300

    clear env

--- src/services/tmdbService.ts
+++ src/services/tmdbService.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const fetchPopularMovies = async (page: number) => {
